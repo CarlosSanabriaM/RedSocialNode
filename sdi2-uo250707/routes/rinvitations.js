5c3839dbdaee9ff3d424efa907756cb08735df99
CarlosSanabriaM <uo250707@uniovi.es>
2018-04-04 20:00:04 +0200

    Se evita que puedas enviarte una invitacion a ti mismo.

--- sdi2-uo250707/routes/rinvitations.js
+++ sdi2-uo250707/routes/rinvitations.js
@@ -1,6 +1,14 @@
 module.exports = function(app, swig, gestorBD) {
 
 	app.get("/user/invitate/:email", function(req, res) {
+		// Comprobamos que el email indicado no coincida con el email del usuario en sesion
+		if(req.params.email === req.session.email){
+			res.redirect("/user/list" +
+	 				"?message=¡No puede enviarte una invitación de amistad a ti mismo!"+
+	 				"&messageType=alert-warning");
+			return;
+		}
+		
 		// Creamos una invitación del usuario en sesión al usuario con el email indicado
 		var invitation = {
 			senderEmail : req.session.email, 
